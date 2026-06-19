b49158bab8f8972310ebc0c9116c2dca77b90e42
Keisho78 <Keisho78@users.noreply.github.com>
2026-06-19 14:33:55 +0900

    Redesign app UI with bottom navigation

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "sanjo-completion-report-v45";
+const CACHE_NAME = "sanjo-completion-report-v46";
 const APP_ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=45",
-  "./app.js?v=45",
+  "./styles.css?v=46",
+  "./app.js?v=46",
   "./manifest.webmanifest",
   "./icons/icon-180.png?v=18",
   "./icons/icon-192.png?v=18",
